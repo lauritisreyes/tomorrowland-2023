bb94acbbbf8411b462c391ad5d2ead630019f394
Diegantos <diegantos8@gmail.com>
2022-12-12 21:59:29 +0100

    First upload

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -10,8 +10,9 @@ export const Intro = () => {
             <div className="Box">
                 <div className="Box-info">
                     <p className="Box-text">People of Tomorrow,</p>
-                    <p className="Box-text">In 2023 you will witness the rise of a magnificent tale in the history of Tomorrowland's Great Library. Our destination lies high on the horizon. Prepare yourself for a beautiful adventure on July 21 & - 23 and July 28 - 30.</p>
-                    <p className="Box-text">Pre-Registration starts on December 7 on <span><a href="#">adscendo.tomorrowland.com.</a></span></p>
+                    <p className="Box-text">In 2023 you will witness the rise of a magnificent tale in the history of Tomorrowland's Great Library. Our destination lies high on the horizon. Prepare yourself for a beautiful adventure on July 21 - 23 and July 28 - 30.</p>
+                    <p className="Box-text">Learn more about Tomorrowland 2023 - Adscendo on <a href="#" className="Box-link">adscendo.tomorrowland.com.</a></p>
+                    <p className="Box-text">Pre-register for the Tomorrowland 2023 ticket sales in your <a href="#" className="Box-link">Tomorrowland Account.</a></p>
                 </div>
                 <div className="Box-weekend">
                     <div className="Box-weekend1">
