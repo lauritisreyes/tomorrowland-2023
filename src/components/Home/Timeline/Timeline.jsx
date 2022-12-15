7bdd3843df04aa2de966c1c5e9ddcc3d44a64cf0
Diegantos <diegantos8@gmail.com>
2022-12-15 12:17:55 +0100

    Arrays

--- src/components/Home/Timeline/Timeline.jsx
+++ src/components/Home/Timeline/Timeline.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import { Year } from '../Common/Common'
 import './Timeline.scss'
 
 export const Timeline = () => {
 
+    const [ position, setPosition ] = useState(0)
+    const change = ( valor ) => setPosition( valor )
 
     const Grid = [
         {id : 0, year : '2022', title : 'The Reflection of Love', text : 'In 2022, Tomorrowland made its grand return to the beautiful and unique scenery of De Schorre, welcoming more than 600,000 People of Tomorrow to celebrate together during 3 wonderful weekends of magic. The 16th edition of Tomorrowland revolved around a magnificent new tale ‘The Reflection of Love’, symbolizing the positive energy the festival stands for. The whole world united again in one magical place filled with energy and love, embracing the gift of life and reflecting all that is positive from around the globe while enjoying mesmerizing performances of more than 800 of the planet’s finest electronic artists, including Alesso, Amelie Lens, Armin van Buuren, Charlotte de Witte, Dimitri Vegas & Like Mike, Diplo, Eric Prydz, Lost Frequencies, Marshmello, Martin Garrix, Paul Kalkbrenner and many more…'},
-        // {id : 1, year : '2021', title : 'Arround the world', text : 'Following the huge success of its first ever digital edition in 2020, Tomorrowland brought back the spectacular two-day digital music festival experience on July 16 & 17, 2021, welcoming the most spectacular group of entertainers in the universe ‘The Amicorum Spectaculum’. The magnificent second edition of Tomorrowland – Around the World was adapted to all time zones and brought together a stellar line-up featuring more than 40 of the planet’s biggest names in electronic dance music, while the People of Tomorrow from all corners of the globe united again as one in one place – celebrating the spectacle of friendship.'},
     ]
 
     const Slides = [
@@ -19,7 +21,6 @@ export const Timeline = () => {
         { id : 6, url : '/assets/2022-7.jpg', title : 'Photo7'},
         { id : 7, url : '/assets/2022-8.jpg', title : 'Photo8'},
         { id : 8, url : '/assets/2022-9.jpg', title : 'Photo9'},
-        { id : 9, url : '/assets/2022-1.jpg', title : 'Photo1'},
     ]
 
     return(
@@ -64,6 +65,7 @@ const Slider = () => {
             <div className="Slider-container">
                 <div className="Slider-hidden">
                     <ul className="Slider-img">
+                        {/* <desplazarMas /> */}
                         <li className="Slider-photo">
                             <img src="/assets/2022-1.jpg" alt="Img" loading='lazy' />
                         </li>
@@ -92,7 +94,8 @@ const Slider = () => {
                             <img src="/assets/2022-9.jpg" alt="Img" loading='lazy' />
                         </li>
                     </ul>
-                    <ul className="Slider-control">
+
+                    <ul onClick={ ()=> change(id) } className="Slider-control">
                         <li className="Slider-control-part"></li>
                         <li className="Slider-control-part"></li>
                         <li className="Slider-control-part"></li>
@@ -104,11 +107,13 @@ const Slider = () => {
                         <li className="Slider-control-part"></li>
                     </ul>
                 </div>
-                <div className="Slider-btn Slider-btn-left">
+
+                <div onClick={ desplazarMas }  className="Slider-btn Slider-btn-left">
                     <svg className='Slider-svg' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                     </svg>
                 </div>
+
                 <div className="Slider-btn Slider-btn-right">
                     <svg className='Slider-svg' xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
@@ -117,5 +122,22 @@ const Slider = () => {
             </div>
 
         </div>
+    )
+}
+
+const desplazarMas = () => {
+
+    position = id * -11.11
+
+    position++
+
+    // {Slides.map( ({ id, url, title})=>
+        
+    // )}
+
+    return(
+        <li className="Slider-photo">
+            <img key={id} src={url} alt={title} loading='lazy' />
+        </li>
     )
 }
