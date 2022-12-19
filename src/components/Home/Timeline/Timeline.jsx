6c747419fe19fffdaa08458885e7343a52635e49
Diegantos <diegantos8@gmail.com>
2022-12-19 16:18:49 +0100

    new

--- src/components/Home/Timeline/Timeline.jsx
+++ src/components/Home/Timeline/Timeline.jsx
@@ -4,8 +4,40 @@ import './Timeline.scss'
 
 export const Timeline = () => {
 
-    const Grid = [
-        {id : 0, year : '2022', title : 'The Reflection of Love', text : 'In 2022, Tomorrowland made its grand return to the beautiful and unique scenery of De Schorre, welcoming more than 600,000 People of Tomorrow to celebrate together during 3 wonderful weekends of magic. The 16th edition of Tomorrowland revolved around a magnificent new tale ‘The Reflection of Love’, symbolizing the positive energy the festival stands for. The whole world united again in one magical place filled with energy and love, embracing the gift of life and reflecting all that is positive from around the globe while enjoying mesmerizing performances of more than 800 of the planet’s finest electronic artists, including Alesso, Amelie Lens, Armin van Buuren, Charlotte de Witte, Dimitri Vegas & Like Mike, Diplo, Eric Prydz, Lost Frequencies, Marshmello, Martin Garrix, Paul Kalkbrenner and many more…'},
+    
+    const timelineItems = [
+        { 
+            id : 0 ,
+            year : '2022',
+            title : 'The Reflection of Love',
+            text : 'In 2022, Tomorrowland made its grand return to the beautiful and unique scenery of De Schorre, welcoming more than 600,000 People of Tomorrow to celebrate together during 3 wonderful weekends of magic. The 16th edition of Tomorrowland revolved around a magnificent new tale ‘The Reflection of Love’, symbolizing the positive energy the festival stands for. The whole world united again in one magical place filled with energy and love, embracing the gift of life and reflecting all that is positive from around the globe while enjoying mesmerizing performances of more than 800 of the planet’s finest electronic artists, including Alesso, Amelie Lens, Armin van Buuren, Charlotte de Witte, Dimitri Vegas & Like Mike, Diplo, Eric Prydz, Lost Frequencies, Marshmello, Martin Garrix, Paul Kalkbrenner and many more…',
+            slides : [
+                { id : 0, url : '/assets/2022-1.jpg', title : 'Photo1'},
+                { id : 1, url : '/assets/2022-2.jpg', title : 'Photo2'},
+                { id : 2, url : '/assets/2022-3.jpg', title : 'Photo3'},
+                { id : 3, url : '/assets/2022-4.jpg', title : 'Photo4'},
+                { id : 4, url : '/assets/2022-5.jpg', title : 'Photo5'},
+                { id : 5, url : '/assets/2022-6.jpg', title : 'Photo6'},
+                { id : 6, url : '/assets/2022-7.jpg', title : 'Photo7'},
+                { id : 7, url : '/assets/2022-8.jpg', title : 'Photo8'},
+                { id : 8, url : '/assets/2022-9.jpg', title : 'Photo9'},
+            ]
+        },
+        { 
+            id : 1 ,
+            year : '2021',
+            title : 'Arround the World',
+            text : 'Following the huge success of its first ever digital edition in 2020, Tomorrowland brought back the spectacular two-day digital music festival experience on July 16 & 17, 2021, welcoming the most spectacular group of entertainers in the universe ‘The Amicorum Spectaculum’. The magnificent second edition of Tomorrowland – Around the World was adapted to all time zones and brought together a stellar line-up featuring more than 40 of the planet’s biggest names in electronic dance music, while the People of Tomorrow from all corners of the globe united again as one in one place – celebrating the spectacle of friendship.',
+            slides : [
+                { id : 0, url : '/assets/2021-1.jpg', title : 'Photo1'},
+                { id : 1, url : '/assets/2021-2.jpg', title : 'Photo2'},
+                { id : 2, url : '/assets/2021-3.jpg', title : 'Photo3'},
+                { id : 3, url : '/assets/2021-4.jpg', title : 'Photo4'},
+                { id : 4, url : '/assets/2021-5.jpg', title : 'Photo5'},
+                { id : 5, url : '/assets/2021-6.jpg', title : 'Photo6'},
+                { id : 6, url : '/assets/2021-7.jpg', title : 'Photo7'},
+            ]
+        },
     ]
 
     return(
@@ -17,28 +49,22 @@ export const Timeline = () => {
                 <p className="Timeline-p">TOMORROWLAND THROUGH THE YEARS</p>
             </div>
 
-            <div className="Timeline-item">
-                <Year clase='Timeline-year'>2022</Year>
-                <div className="Timeline-container">
-                    <div className="Timeline-info">
-                        {Grid.map( info =>
-                            <Info key={ info.id } {...info}/>
-                        )}
+            { timelineItems.map( item =>
+
+                <div className="Timeline-item">
+                    <Year clase='Timeline-year'>{ item.year }</Year>
+                    <div className="Timeline-container">
+                        <div className="Timeline-info">
+                        {/* <Info year={item.year} title={item.title} text={item.text} /> */}
+                        <Info { ...item }></Info>
                     </div>
-                    <Slider />
+                    {/* <Slider slides={ item.slides } /> */}
+                    <Slider {...item} />
                 </div>
             </div>
-            {/* <div className="Timeline-item">
-                <Year clase='Timeline-year'>2021</Year>
-                <div className="Timeline-container">
-                    <div className="Timeline-info">
-                        {Grid.map( info =>
-                            <Info key={ info.id } {...info}/>
-                        )}
-                    </div>
-                    <Slider />
-                </div>
-            </div> */}
+            )}
+            
+            
 
         </div>
 
@@ -55,28 +81,16 @@ const Info = ({ year, title, text}) => {
     )
 }
 
-const Slider = () => {
+const Slider = ({ slides }) => {
 
     const [ foto, setFoto ] = useState(0)
     const change = ( valor ) => setFoto( valor )
     
-    const slides = [
-        { id : 0, url : '/assets/2022-1.jpg', title : 'Photo1'},
-        { id : 1, url : '/assets/2022-2.jpg', title : 'Photo2'},
-        { id : 2, url : '/assets/2022-3.jpg', title : 'Photo3'},
-        { id : 3, url : '/assets/2022-4.jpg', title : 'Photo4'},
-        { id : 4, url : '/assets/2022-5.jpg', title : 'Photo5'},
-        { id : 5, url : '/assets/2022-6.jpg', title : 'Photo6'},
-        { id : 6, url : '/assets/2022-7.jpg', title : 'Photo7'},
-        { id : 7, url : '/assets/2022-8.jpg', title : 'Photo8'},
-        { id : 8, url : '/assets/2022-9.jpg', title : 'Photo9'},
-    ]
 
     const desplazarMenos = () => {
         
         let indice = foto === 0 ? slides.length - 1 : foto - 1
         setFoto( indice )
-        // console.log(indice)
     }
     
 
@@ -84,7 +98,6 @@ const Slider = () => {
 
         let indice = foto === slides.length - 1 ? 0 : foto + 1
         setFoto ( indice )
-        // console.log(indice)
 
     }
 
@@ -95,6 +108,7 @@ const Slider = () => {
                     <ul 
                         className="Slider-img"
                         style={ {
+                            width : `${ slides.length * 100 }%`,
                             transform : `translateX(-${ 100 / slides.length * foto}%)`
                         } }>
                         {slides && slides.map( ({ id, url, title }) =>
